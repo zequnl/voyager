d3381aaa78077017692a85b06ca4ce8ec726edbc
zequnl <30546310+zequnl@users.noreply.github.com>
2017-12-24 13:11:37 +0800

    Update laser_scan.hpp

--- include/voyager/laser_scan.hpp
+++ include/voyager/laser_scan.hpp
@@ -1,36 +1,3 @@
-
-// MIT License
-
-// Copyright (c) 2017 Neel Parikh
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-/**
- * @file    laser_scan.hpp
- * @author  nr-parikh
- * @copyright MIT license (c) 2017 Neel Parikh
- *
- * @brief DESCRIPTION
- * Header file for the class LaserScan.
- *
- */
-
 #ifndef INCLUDE_VOYAGER_LASER_SCAN_HPP_
 #define INCLUDE_VOYAGER_LASER_SCAN_HPP_
 
@@ -39,35 +6,10 @@
 
 class LaserScan {
  public:
-  /**
-   * @brief Constructor of the class
-   * @details Initialize node handle as well as set is_running_ flag to true
-   */
   LaserScan();
-  /**
-   * @brief Destructor of the class
-   * @details Write actions that needs to be done when destroying the object
-   */
   ~LaserScan();
-  /**
-   * @brief Callback function
-   * @details Callback function for LaserScan messages. Check if there is any
-   * collision.
-   *
-   * @param scan_msg Incoming message of type sensor_msgs/LaserScan
-   */
   auto scanCallback(const sensor_msgs::LaserScan scan_msg) -> void;
-  /**
-   * @brief Check if running
-   * @details Set to true as long as the node is running
-   * @return bool: Return true if running
-   */
   auto isAlive() -> bool;
-  /**
-   * @brief Return the obstacle flag
-   * @details Return true if the obstacle is present, false otherwise
-   * @return bool: Return the value of flag
-   */
   auto checkCollision() -> bool;
 
  private:
