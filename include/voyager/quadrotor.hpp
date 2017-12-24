14f4f4e217216c0ce9cef4c6f7da01662f3b0856
zequnl <30546310+zequnl@users.noreply.github.com>
2017-12-24 13:18:56 +0800

    Update quadrotor.hpp

--- include/voyager/quadrotor.hpp
+++ include/voyager/quadrotor.hpp
@@ -1,36 +1,3 @@
-
-// MIT License
-
-// Copyright (c) 2017 Neel Parikh
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-/**
- * @file    quadrotor.hpp
- * @author  nr-parikh
- * @copyright MIT license (c) 2017 Neel Parikh
- *
- * @brief DESCRIPTION
- * Header file for the classs Quadrotor.
- *
- */
-
 #ifndef INCLUDE_VOYAGER_QUADROTOR_HPP_
 #define INCLUDE_VOYAGER_QUADROTOR_HPP_
 
@@ -42,65 +9,15 @@
 
 class Quadrotor {
  public:
-  /**
-   * @brief Constructor of the class
-   * @details Construct an abject of the class
-   */
   Quadrotor();
-  /**
-   * @brief Destructor of the class
-   * @details Take actions that needs to be done while destroying
-   */
   ~Quadrotor();
-  /**
-   * @brief Explore the environment
-   * @details Allow the robot to move and map the environment
-   * @return void: Return nothing
-   */
   auto exploreAndMap() -> void;
-  /**
-   * @brief Check if running
-   * @details Set to true as long as the node is running
-   * @return bool: Return true if running
-   */
   auto isAlive() -> bool;
-  /**
-   * @brief Callback function
-   * @details Callback function to get height of the robot from the ground
-   *
-   * @param height_msg: Message being received
-   * @return void: Return nothing
-   */
   auto heightCallback(const sensor_msgs::Range height_msg) -> void;
-  /**
-   * @brief Get height from ground
-   * @details Function to get the height of the robot from ground
-   * @return float: Height
-   */
   auto getHeight() -> float;
-  /**
-   * @brief Callback function for service
-   * @details Function callback for the service explore used by voyager
-   *
-   * @param request: Request of the service
-   * @param resp: Response of the service
-   *
-   * @return bool: Return true if succeeded
-   */
   auto explore(voyager::explore::Request& request,
                voyager::explore::Response& resp) -> bool;
-
-  /**
-   * @brief Get explore flag
-   * @details Get the value of the explore flag set in service callback
-   * @return bool: Return value of the flag
-   */
   auto getExplorerFlag() -> bool;
-  /**
-   * @brief Stop the robot
-   * @details Function to stop the robot
-   * @return void: Return nothing
-   */
   auto stop() -> void;
 
  private:
